e0a2baebd52deceb961845f061a1c78bb0cb7c59
Christian Palazzo <palazzochristian@yahoo.it>
2024-04-20 14:13:38 +0200

    encrypt message implementation

--- Crypto.py
+++ Crypto.py
@@ -1,13 +1,14 @@
 import fire
 from primitives.sha import sha3_224, sha3_256, sha3_512
-from primitives.authenticated_encryption import generate_key
+from primitives.authenticated_encryption import generate_key, AESGCM_encrypt
 
 class Crypto(object):
     """Exposes cryptographic methods"""
     
-    sha3_224 = sha3_224
-    sha3_256 = sha3_256
-    sha3_512 = sha3_512
+    sha3_224       = sha3_224
+    sha3_256       = sha3_256
+    sha3_512       = sha3_512
+    AESGCM_encrypt = AESGCM_encrypt
     
     generate_key = generate_key
     
--- primitives/authenticated_encryption.py
+++ primitives/authenticated_encryption.py
@@ -6,9 +6,41 @@ def generate_key(self, key_length):
             raise ValueError("The key length must be 128, 192 or 256 bit")
 
     key = AESGCM.generate_key(bit_length=key_length)
+
     return _bytes_to_hex_string(key)
+
+def AESGCM_encrypt(self, key, secret, unencrypted_data = None):
+    if len(key) != 64:
+        raise ValueError("The key must be 256 bit length")
+    
+    bkey = _hex_string_to_bytes(key)
+    bsecret = _to_bytes_like(secret)
+    bud = _to_bytes_like(unencrypted_data) if unencrypted_data else None
+    
+    aesgcm = AESGCM(bkey)
+    nonce = os.urandom(12)
+    bencrypted = aesgcm.encrypt(nonce, bsecret, bud)
+
+    return _bytes_to_hex_string(bencrypted)
+
+def AESGCM_decrypt(self, key):
+    return "to do"
 
 
 def _bytes_to_hex_string(byte_key):
     hex_string = byte_key.hex()
     return hex_string
+
+def _hex_string_to_bytes(hex_string):
+    byte_key = bytes.fromhex(hex_string)
+    return byte_key
+
+def _to_bytes_like(data):
+    if isinstance(data, str):
+        byte_data = data.encode()
+    elif isinstance(data, (list, tuple)):
+        byte_data = bytes(data)
+    else:
+        raise TypeError("The data type is not supported. Supported types are: string, list and tuple")
+    
+    return byte_data
