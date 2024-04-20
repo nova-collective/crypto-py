94b95f15e6e54dd15ed373ad892bad636af903d7
Christian Palazzo <palazzochristian@yahoo.it>
2024-04-20 15:07:30 +0200

    added comments

--- primitives/sha.py
+++ primitives/sha.py
@@ -2,40 +2,58 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 
 def sha3_224(self, input):
-    # it converts the string into bytes
+    '''
+        This function is used to produce a SHA-3 hash of 224 bites length from the data passed in input.
+
+        Parameters:
+        input (string): The data to hash
+
+        Returns:
+        string: the hash as hexadecimal string  
+    '''
     input_bytes = input.encode('utf-8')
         
-    # Calculates the SHA-3 (SHA3-256) hash of the string in byte
     digest = hashes.Hash(hashes.SHA3_224(), backend=default_backend())
     digest.update(input_bytes)
     hashed_value = digest.finalize()
-    
-    # Converts the hash to hexadecimal format
     hashed_hex = hashed_value.hex()
+    
     return hashed_hex
 
 def sha3_256(self, input):
-    # it converts the string into bytes
+    '''
+        This function is used to produce a SHA-3 hash of 256 bites length from the data passed in input.
+
+        Parameters:
+        input (string): The data to hash
+
+        Returns:
+        string: the hash as hexadecimal string  
+    '''
     input_bytes = input.encode('utf-8')
         
-    # Calculates the SHA-3 (SHA3-256) hash of the string in byte
     digest = hashes.Hash(hashes.SHA3_256(), backend=default_backend())
     digest.update(input_bytes)
     hashed_value = digest.finalize()
-    
-    # Converts the hash to hexadecimal format
     hashed_hex = hashed_value.hex()
+
     return hashed_hex
 
 def sha3_512(self, input):
-    # it converts the string into bytes
+    '''
+        This function is used to produce a SHA-3 hash of 512 bites length from the data passed in input.
+
+        Parameters:
+        input (string): The data to hash
+
+        Returns:
+        string: the hash as hexadecimal string  
+    '''
     input_bytes = input.encode('utf-8')
-        
-    # Calculates the SHA-3 (SHA3-256) hash of the string in byte
+
     digest = hashes.Hash(hashes.SHA3_512(), backend=default_backend())
     digest.update(input_bytes)
     hashed_value = digest.finalize()
-    
-    # Converts the hash to hexadecimal format
     hashed_hex = hashed_value.hex()
+
     return hashed_hex
